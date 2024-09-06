b1b526398ce2d66cf3d4a169c74a5330f85c5d54
minhdat2321 <minhsu372003@gmail.com>
2024-09-06 12:42:21 +0700

    test

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import fmpsdk
 
 
-st.write("Hello")
+dt = fmpsdk.balance_sheet_statement(apikey="xxUbfrh3hsbcydCGtbYZxqpH1jaufUzB", symbol='AAPL')
+st.write(dt)
